8bbfae1c5f2cfb68d26ccfddea1a93b723d8f6b6
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-10-18 09:14:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find Length of Linked List/find-length-of-linked-list.cpp
@@ -0,0 +1,25 @@
+
+// class Node {
+//   public:
+//     int data;
+//     Node *next;
+
+//     Node(int x) {
+//         data = x;
+//         next = NULL;
+//     }
+// };
+
+class Solution {
+  public:
+    int getCount(Node* head) {
+        int count =0;
+        Node *temp = head;
+        while(temp){
+            count++;
+            temp= temp->next;
+        }
+        return count;
+        
+    }
+};
