2fa78ef6aa9ca86b68bd11c7371eacfca71bdda4
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-10-26 03:15:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print 1 to n without using loops/print-1-to-n-without-using-loops.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printTillN(int n) {
+        if(n<=0){
+            return;
+        }
+        else{
+            printTillN(n-1);
+            cout<<n<<" ";
+        }
+        
+    }
+};
