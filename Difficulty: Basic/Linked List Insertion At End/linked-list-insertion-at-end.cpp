f1a7239db18090f448bb4932a0d43d1cf4d2c443
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-10-30 03:08:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
@@ -0,0 +1,36 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        if(head==NULL){
+            head=new Node(x);
+            return head;
+        }
+        Node*temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        Node* newN=new Node(x);
+        temp->next=newN;
+        return head;
+        
+    }
+};
