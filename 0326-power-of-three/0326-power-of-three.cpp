76798ed110844d12fb196e703eb90f183b59dcdc
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-11-07 23:20:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (44.66%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1){return true;}
+        long long x=1;
+        for(int i=1;x<=n;i++){
+            x*=3;
+            if(x==n){
+                return true;
+            }
+
+        }
+        return false;
+        
+    }
+};
