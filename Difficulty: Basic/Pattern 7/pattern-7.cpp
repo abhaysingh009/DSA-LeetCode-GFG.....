d267705b5eed2d05bd8d3acc21448d427751fd3e
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-11-08 05:16:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 7/pattern-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            
+            for (int k = i; k < n; k++) {
+                cout<<" ";
+            }
+            
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+            
+            
+        }
+        cout<<"\n";
+        }
+    }
+};
