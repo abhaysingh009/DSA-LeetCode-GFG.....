2b01d1519089eed42127b87949ceb7e6a7f00376
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-11-13 00:47:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Quick Left Rotation/quick-left-rotation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+
+  public:
+    void leftRotate(vector<int>& arr, int d) {
+        int n = arr.size();
+    d = d % n; // in case d > n
+    reverse(arr.begin(), arr.begin() + d);
+    reverse(arr.begin() + d, arr.end());
+    reverse(arr.begin(), arr.end());
+    }
+};
