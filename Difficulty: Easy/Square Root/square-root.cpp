129c6e0f7073b40cc29ad1fa6174d5aef4a10068
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-11-18 16:32:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        int x=0;
+        for(int i=1;i*i<=n;i++){
+            x=i;
+            
+        }
+        return x;
+        
+    }
+};
