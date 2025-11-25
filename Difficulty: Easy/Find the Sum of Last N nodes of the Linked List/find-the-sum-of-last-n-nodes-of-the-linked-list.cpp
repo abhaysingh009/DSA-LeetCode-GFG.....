fdae6fa4dbf82e9197efb1365b26a12ba333765a
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-11-26 01:06:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    /*Structure of the node of the linled list is as
+
+    struct Node {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+    };
+    */
+    // your task is to complete this function
+    // function should return sum of last n nodes
+    int sumOfLastN_Nodes(struct Node* head, int n) {
+        Node*temp=head;
+
+        int count=0;
+        while(temp){
+            
+            count++;
+            temp=temp->next;
+        }
+        int sum=0;
+        temp=head;
+        while(temp){
+            count--;
+            
+            if(count<n){
+                sum+=temp->data;
+            }
+            temp=temp->next;
+        }
+        return sum;
+  
+    }
+};
