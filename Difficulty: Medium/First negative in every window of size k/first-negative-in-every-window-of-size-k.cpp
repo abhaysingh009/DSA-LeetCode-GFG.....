8d725a5aec7fb435c69e826cdec2ff72c7a2f2b5
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-11-30 00:34:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+       vector<int> ans;
+        int n = arr.size();
+
+        queue<int> q;
+        for (int i = 0; i < k - 1; i++) if(arr[i]<0)q.push(i);
+
+        for (int i = k - 1; i < n; i++) {
+            if(arr[i]<0){
+            q.push(i);
+            }
+            if(q.empty()){
+                ans.push_back(0);
+            }
+            else{
+                if(q.front()<=i-k){
+                    q.pop();
+                }
+                if(q.empty()){
+                      ans.push_back(0);
+                }
+                else{
+                      ans.push_back(arr[q.front()]);
+                }
+            }
+        }
+        return ans;
+    }
+};
