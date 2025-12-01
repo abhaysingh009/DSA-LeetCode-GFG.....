cd7acc40c6d61760d08b0d2c421b3ca6a1934fe9
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-12-01 14:23:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Palindrome Array/palindrome-array.cpp
+++ Difficulty: Basic/Palindrome Array/palindrome-array.cpp
@@ -2,15 +2,21 @@
 class Solution {
   public:
     bool isPerfect(vector<int> &arr) {
-        vector<int> arr2(arr.begin(),arr.end());
-        reverse(arr2.begin(),arr2.end());
-        for(int i=0;i<arr.size();i++){
-            if (arr[i]!=arr2[i]){
-                return false;
-            }
+        // vector<int> arr2(arr.begin(),arr.end());
+        // reverse(arr2.begin(),arr2.end());
+        // for(int i=0;i<arr.size();i++){
+        //     if (arr[i]!=arr2[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        int n=arr.size();
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr[n-1-i]){
+            return false;
         }
-        return true;
-
+    }
+    return true;
         
     }
 };
