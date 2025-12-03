18d7cc8d233279757839d95ca4eb87d8a7878ab0
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-12-03 18:34:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Binary Array Sorting/binary-array-sorting.cpp
+++ Difficulty: Medium/Binary Array Sorting/binary-array-sorting.cpp
@@ -1,22 +1,21 @@
 class Solution {
   public:
     void binSort(vector<int> &arr) {
-        int n=arr.size();
         int i=0;
-        int j=n-1;
+        int j=arr.size()-1;
         
-        while(i<=j){
-            if(arr[i]==1 and arr[j]==0){
-                swap(arr[i],arr[j]);
-                i++;
-                j--;
-            }
-            else if(arr[i]==0){
-                i++;
-            }
-            else{
-                j--;
-            }
+        while(i<j){
+        if(arr[i]==1 and arr[j]==0){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+        else if(arr[i]==0){
+            i++;
+        }
+        else if(arr[j]==1){
+            j--;
+        }
         }
         
     }
