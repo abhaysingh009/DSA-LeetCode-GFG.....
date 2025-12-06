588e1168dedb2276d15d43afc3c3955cd8c90876
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-12-06 12:20:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<int>st;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty())return false;
+                
+                if(s[i]==')'){
+                    if(st.top()=='('){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                if(s[i]=='}'){
+                     if(st.top()=='{'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                    
+                }if(s[i]==']'){
+                     if(st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                    
+              
+            }
+        }
+        }
+        return st.empty();
+        
+    }
+};
