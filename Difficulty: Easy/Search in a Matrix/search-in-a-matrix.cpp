2d2773e774aab0042c6131df2dbdd6a37ca01e1a
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-12-26 16:20:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Matrix/search-in-a-matrix.cpp
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.cpp
@@ -2,19 +2,16 @@
 class Solution {
   public:
     // Function to search a given integer in a matrix.
-    bool searchMatrix(vector<vector<int>> &arr, int x) {
-        int m=arr.size();
-        int n=arr[0].size();
-        
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        int m=mat.size();
+        int n=mat[0].size();
         for(int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                if(arr[i][j]==x){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==x){
                     return true;
                 }
             }
         }
         return false;
-        
-        
     }
 };
