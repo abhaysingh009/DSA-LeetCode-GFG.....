f3f2fb0a58e811712be57dbabe234226a0d3501e
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-01-18 15:28:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.79%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int upper=0;
+       for(char c:word){
+        if(c>='A' and c<='Z'){
+            upper++;
+        }
+       }
+       if(upper==word.size())return true;
+       if(upper==0)return true;
+       if(upper==1 and word[0]>='A' and word[0]<='Z')return true;
+
+        return false;
+    }
+};
