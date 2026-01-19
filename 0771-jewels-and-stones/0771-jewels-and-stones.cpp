efb12a1947fa1feb6c44774089d6e5828aa35c6b
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-01-19 19:03:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int numJewelsInStones(string jwels, string stones) {
-        // vector<int>res(26,0);
+        
         int res=0;
-        set<char>st(jwels.begin(),jwels.end());
         for(char c:stones){
-            if(st.contains(c)){
+            if(jwels.contain(c)){
                 res++;
             }
         }
