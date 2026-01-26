bc74763261f158308c819687e8fe3beda4ccb6f8
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-01-26 17:13:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 1/pattern-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void printSquare(int n) {
+        for(int i=0;i<n;i++){
+            for(int i=0;i<n;i++){
+                cout<<"* ";
+            }
+            cout<<"\n";
+        }
+        
+    }
+};
