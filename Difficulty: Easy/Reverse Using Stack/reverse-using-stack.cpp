e55ebcc038d42ff129c464bfc205d2febb1052a5
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-03-24 12:44:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    string reverse(const string& S) {
+        stack<char>st;
+        for(char c:S){
+            st.push(c);
+        }
+        
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
