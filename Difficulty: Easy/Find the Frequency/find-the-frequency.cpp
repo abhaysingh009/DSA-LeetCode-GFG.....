d9853b90f6238c9d57c79150bbe3207fa1fa979a
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-03-28 21:18:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.cpp
@@ -0,0 +1,16 @@
+/*Function to find frequency of x
+ * x : element whose frequency is to be found
+ * arr : input vector
+ */
+class Solution {
+  public:
+    int findFrequency(vector<int> arr, int x) {
+        int ans=0;
+        for(int i:arr){
+            if(i==x){
+                ans++;
+            }
+        }
+        return ans;        
+    }
+};
