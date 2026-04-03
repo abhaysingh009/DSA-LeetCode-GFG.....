831ce4985cda74c0f9679f75a98af588ef0c21f5
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-04-03 14:14:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (48.53%) - LeetHub

--- 3707-equal-score-substrings/3707-equal-score-substrings.cpp
+++ 3707-equal-score-substrings/3707-equal-score-substrings.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     bool scoreBalance(string s) {
-        vector<int>ans;
-        for(char c:s){
-            ans.push_back((c-'a')+1);
+        int total=0;
+    for(char c:s){
+            total=total+(c-'a')+1;
         }
-        
-    int x=0;
-    int y=ans.size()-1;
-    int l=0;
-    int r=0;
-    while(x<y){
-        l+=ans[x++];
-        r+=ans[y--];
-        if(l==r){
-            
-            return 1; 
+    int left=total;
+    int right=0;
+    for(int i=0;i<s.size();i++){
+        left-=(s[i]-'a')+1;
+        right+=(s[i]-'a')+1;
+        if(left==right){
+            return 1;
         }
-        
     }
     return 0;
-
+        
     }
 };
