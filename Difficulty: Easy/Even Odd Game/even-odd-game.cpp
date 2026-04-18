0efa402b21f0170700914e02729fcb1e9540c42f
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-04-18 14:21:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Friend";
+    }else{
+        cout<<"You";
+    }
+    
+
+    return 0;
+}
