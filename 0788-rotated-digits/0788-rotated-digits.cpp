a47b49049487f72db7cec5bfa00ad09454454b2a
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-02 11:33:58 +0530

    Time: 78 ms (5.09%), Space: 39.6 MB (5.09%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -3,30 +3,21 @@ public:
 bool helper(int n){
     int x=n;
     set<int>st={3,4,7};
-    string temp="";
+    bool changed =false;
  
     while(x>0){
         int rem=x%10;
         if(st.count(rem)){
             return 0;
         }
-        switch(rem){
-            case 2:
-            rem=5;break;
-            case 5:
-            rem=2;break;
-            case 6:
-            rem=9;break;
-            case 9:
-            rem=6;break;
+        if(rem==2||rem==5||rem==6||rem==9){
+            changed=true;
         }
-        temp+=to_string(rem);
+        
         x/=10;
 
     }
-    reverse(temp.begin(),temp.end());
-    if(stoi(temp)==n)return 0;
-    return 1;
+    return changed;
 }
     int rotatedDigits(int n) {
         int count=0;
