5e984def31277446289878701f7a9402d9ae0af2
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-07 19:23:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common Elements/common-elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> commonElements(vector<int> &a, vector<int> &b) {
+        
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        
+        vector<int> ans;
+        
+        int i = 0, j = 0;
+        
+        while(i < a.size() && j < b.size()) {
+            
+            if(a[i] == b[j]) {
+                ans.push_back(a[i]);
+                i++;
+                j++;
+            }
+            else if(a[i] < b[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};
