e3ee3436e59939e65796cb4fdd8cf124e93ca732
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-14 19:14:23 +0530

    Time: 4 ms (12.92%), Space: 26.8 MB (49.61%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n=nums.size();
+        int m=nums[0];
+        for(int i:nums){
+            m=max(m,i);
+        }
+
+        if(m+1!=n||m==0)return 0;
+
+        unordered_map<int,int>freq;
+        for(int i:nums){
+            freq[i]++;
+        }
+        for(auto it:freq){
+            if(it.first!=m){
+                if(it.second!=1)return 0;
+            }else{
+                if(it.second!=2)return 0;
+            }
+        }
+        return 1;
+        
+    }
+};
