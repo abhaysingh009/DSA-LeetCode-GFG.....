25d180cceffc94d7b324de9ca73bab9f51381c89
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-17 13:46:41 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.02%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+int helper(int m,int n,vector<vector<int>>&dp){
+    if(m<0||n<0)return 0;
+    if(m==0 and n==0)return 1;
+    if(dp[m][n]!=-1)return dp[m][n];
+    return dp[m][n]=helper(m-1,n,dp)+helper(m,n-1,dp);
+}
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+       return helper(m-1,n-1,dp);
+    }
+};
