c4cebaf4997b1a216795f3cc43b0bc0efb1670c2
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-18 18:22:25 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
         stack<char>st;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='['|| s[i]=='{' ){
-                st.push(s[i]);
-
-            }
+            if(s[i]=='(' || s[i]=='{' ||s[i]=='[')st.push(s[i]);
             else{
-                if(st.empty()){return false;}
-                else if( st.top()=='(' and s[i]==')'||
-                    st.top()=='[' and s[i]==']'||
-                    st.top()=='{' and s[i]=='}'){
-                        st.pop();
-                        }
-                       
+                if(st.empty())return 0;
+                if(s[i]==')'){
+                    if(st.top()=='(')st.pop();
+                    else return 0;
+                }
+                else if(s[i]=='}'){
+                    if(st.top()=='{')st.pop();
+                    else return 0;
+                }
+                else if(s[i]==']'){
+                    if(st.top()=='[')st.pop();
+                    else return 0;
+                }
             }
         }
         return st.empty();
