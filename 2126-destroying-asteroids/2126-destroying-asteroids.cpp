37f05230ac04bd05df8bed61b94ba4581d441e4f
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-31 21:14:55 +0530

    Time: 31 ms (66.42%), Space: 106.7 MB (81.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -5,6 +5,7 @@ public:
         int maximum=100000;
         long long mass=m;
         for(int i=0;i<arr.size();i++){
+            if(mass>=maximum) return 1;
             if(arr[i]>mass)return 0;
             else mass+=arr[i];
         }
