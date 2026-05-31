e3ce435cdb5b3119477e252aede65e7f2dfcdbbb
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-05-31 21:15:26 +0530

    Time: 23 ms (93.28%), Space: 106.7 MB (81.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool asteroidsDestroyed(int m, vector<int>& arr) {
+    bool asteroidsDestroyed(int mass, vector<int>& arr) {
         sort(arr.begin(),arr.end());
         int maximum=100000;
-        long long mass=m;
+        // long long mass=m;
         for(int i=0;i<arr.size();i++){
             if(mass>=maximum) return 1;
             if(arr[i]>mass)return 0;
