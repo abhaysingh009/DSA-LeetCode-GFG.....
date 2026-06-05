e6a21f1b714f9d4959dee687e0f36b64399696ae
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-06-05 10:20:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Toeplitz matrix/toeplitz-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool isToeplitz(vector<vector<int>>& mat) {
+        for(int i=0;i<mat.size()-1;i++){
+            for(int j=0;j<mat[0].size()-1;j++){
+                if(mat[i][j]!=mat[i+1][j+1])return 0;
+            }
+        }
+        return 1;
+        
+    }
+};
