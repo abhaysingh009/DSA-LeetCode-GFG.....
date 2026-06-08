f514cc64e2200c0af5e7821e1e602cd521f642be
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-06-08 21:24:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>=k)return i;
+        }
+        return arr.size();
+        
+    }
+};
