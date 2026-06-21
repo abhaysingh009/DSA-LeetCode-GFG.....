d64cccaa25f8f537daff3236a317d092f03b9356
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-06-21 15:05:53 +0530

    Time: 32 ms (59.54%), Space: 80.1 MB (89.61%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+     sort(costs.begin(),costs.end());
+     int ans=0;
+     int sum=0;
+     int i=0;
+     while(i<costs.size()){
+        sum+=costs[i++];
+        if(sum<=coins)ans++;
+        else break;
+     }   
+     return ans;
+    }
+};
