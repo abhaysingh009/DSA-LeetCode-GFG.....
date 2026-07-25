8bb9282928f64cf51c46bfd2365d476330b3bb3d
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-07-25 23:14:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Matrix/sum-of-matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int sumOfMatrix(vector<vector<int>>& mat) {
+        int sum = 0;
+        
+        for(int i = 0; i < mat.size(); i++) {
+            for(int j = 0; j < mat[i].size(); j++) {
+                sum += mat[i][j];
+            }
+        }
+        
+        return sum;
+    }
+};
