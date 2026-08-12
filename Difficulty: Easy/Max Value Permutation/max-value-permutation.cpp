0e26eaa4464e757f94c21efcc4119ef31d6eb03a
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-08-12 23:45:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Value Permutation/max-value-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int maxValue(vector<int> &arr) {
+        int ans=0;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            ans+=(arr[i]*i);
+        }
+        return ans;
+        
+    }
+};
