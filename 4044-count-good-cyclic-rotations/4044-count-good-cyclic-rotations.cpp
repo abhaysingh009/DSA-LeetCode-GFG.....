181a65caeb2fab33ad5b5c59c1229b295485d771
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-09-07 00:16:08 +0530

    Time: 7 ms (36.86%), Space: 103.2 MB (66.49%) - LeetHub

---
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countGoodRotations(vector<int>& nums) {
+        long long sum=0;
+        int n=nums.size();
+       
+        long long half=0;
+        for(int i=0;i<n;i++) {
+            sum+=nums[i];
+            if(i<n/2)
+            half+=nums[i];
+        }
+        int ans=0;
+        if(half>(sum-half))ans=1;
+        int k=n/2;
+        for(int i=1;i<nums.size();i++){
+                half+=(nums[(i+k-1)%n]);
+                half-=nums[i-1];
+                if(half>(sum-half))ans++;
+
+        }     
+        return ans;
+    }
+};
