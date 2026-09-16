bc73ad8933e213ad558d8ec718425d969469dc04
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-09-16 20:39:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.93%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    const long long M = 1000000007;
-long long power(long long a ,long long b){
-    long long ans=1;
-    while(b>0){
-        if(b&1){
-           ans=(ans*a)%M;
-        }
-        a=(a*a)%M;
-        b>>=1;
-    }
-    return ans;
+long long mod=1e9+7;
+long long power(long long n,long long x){
+    if(x==0)return 1;
+    int half=power(n,x/2);
+    if(x%2==0){
+        return (half%mod * half%mod)%mod;
+    }else return (n%mod * half%mod * half%mod)%mod;
 }
-
     int countGoodNumbers(long long n) {
-       int e=5;
-       int p=4;
-       long long  b=n/2;
-       if(n%2!=0)b=(n-(n/2));
-       long long even=power(e,b);
-       long long prime=power(p,n/2);
-       long long ans=(even%M * prime%M) %M;
-       return ans;
+        long long a=power(5,(n+1)/2);
+        long long b=power(4,n/2);
+        return (1LL*a%mod * b%mod)%mod;
+        
     }
 };
