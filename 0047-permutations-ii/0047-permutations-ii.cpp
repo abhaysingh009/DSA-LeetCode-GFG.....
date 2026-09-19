00b13b71ae28a8b42e9067247bc8aaa7b2f406f6
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-09-19 09:57:18 +0530

    Time: 3 ms (60.89%), Space: 11.5 MB (82.78%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+void helper(int i,vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp,vector<bool>&used){     if(temp.size()==nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+    for(int j=i;j<nums.size();j++){
+        if(j>0 and nums[j]==nums[j-1] and !used[j-1])continue;
+        if(!used[j]){
+            temp.push_back(nums[j]);
+            used[j]=1;
+        
+        helper(0,nums,ans,temp,used);
+        used[j]=0;
+        temp.pop_back();
+        }
+
+    }
+}
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        vector<bool>used(n,0);
+        vector<int>temp;
+        int idx=0;
+        sort(nums.begin(),nums.end());
+        helper(idx,nums,ans,temp,used);
+        return ans;
+    }
+};
